76400e9aa68a0f4b8c2657446b508d6808319fbd
Nguyễn Trường Thành <48469016+truongthanh12@users.noreply.github.com>
2021-09-20 16:39:38 +0700

    add form page

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,3 +95,29 @@ window.addEventListener("scroll", (e) => {
 //     getHeightMenu.style.top = "0px";
 //   }
 // });
+
+// inform check step
+function nextStep() {
+  var getStep1 = document.getElementsByClassName('step-1')[0]
+  var getStep2 = document.getElementsByClassName('step-2')[0]
+  var getPrevBtn = document.getElementsByClassName('prev-button')[0]
+  var pagi = document.getElementsByClassName('pagination-circle')[0]
+  var pagi2 = document.getElementsByClassName('pagination-circle')[1]
+  getStep1.classList.add('hide')
+  pagi.classList.remove('active')
+  pagi2.classList.add('active')
+  getPrevBtn.classList.remove('disabled')
+  getStep2.classList.remove('hide')
+}
+function prevStep() {
+  var getStep1 = document.getElementsByClassName('step-1')[0]
+  var getStep2 = document.getElementsByClassName('step-2')[0]
+  var getPrevBtn = document.getElementsByClassName('prev-button')[0]
+  var pagi = document.getElementsByClassName('pagination-circle')[0]
+  var pagi2 = document.getElementsByClassName('pagination-circle')[1]
+  pagi2.classList.remove('active')
+  pagi.classList.add('active')
+  getStep2.classList.add('hide')
+  getPrevBtn.classList.add('disabled')
+  getStep1.classList.remove('hide')
+}
